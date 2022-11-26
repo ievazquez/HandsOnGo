8357c17ca82594895d9a8475c28f5333b1c3d459
Iván Vázquez <ievazquez@hotmail.com>
2022-11-26 19:28:23 +0000

    Final version

--- basics/main.go
+++ basics/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/jboursiquot/go-proverbs"
 )
 
+func getRandomProverb() string {
+	//fmt.Println(proverbs.Random())
+	//fmt.Println(proverbs.Random())
+	//fmt.Println(proverbs.Random())
+	return proverbs.Random().Saying
+}
 
 func main() {
-	  fmt.Println(proverbs.Random())
-	  fmt.Println(proverbs.Random())
-	  fmt.Println(proverbs.Random())
+	fmt.Println(getRandomProverb())
 }
